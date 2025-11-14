6243979b09397659f2cb9c3611f9b6c139fe4fce
Jacques Lavoie <46101717+Stormyisland@users.noreply.github.com>
2025-11-13 22:59:26 -0800

    Add SimpleTransformer class definition
    Initialize the SimpleTransformer model with parameters.

---
+++ main.py
@@ -0,0 +1,14 @@
+import torch 
+import torch.nn as nn
+import math 
+class SimpleTransformer(nn.Module):
+  def __init__(self, vocab_size, d_model = 512, nhead=8, Layers = 6, maxseq_length = 512):
+"""
+Initialize the simple Transformer model.
+
+args: vocab_size : size of the vocabulary (number of unique tokens)
+d_model: Dimension of emmbeding vectors (default 5122)
+nhead : Number of attention headds (default 8)
+num_layers : Number of transformer encoder layers (default 6)
+max_seq_lengt: Maximum sequence length the model can handle (default 512)
+"""
